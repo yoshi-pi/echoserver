44bea2cc81be95f9a8960bb794a6a619c82f6a7a
Yoshipi <yoshipyft@gmail.com>
2022-01-02 19:33:17 +0900

    rewrite test names

--- test/app.test.ts
+++ test/app.test.ts
@@ -2,7 +2,7 @@ import { request } from './request'
 import { imageSize } from 'image-size'
 
 describe('Test the root path', () => {
-  test('It should respond with only headers', async () => {
+  test('It should respond with the specified headers', async () => {
     const { res, body } = await request({
       method: 'get',
       path: '/?query={ "headers": {"content-type": "application/json", "a": "a"}}'
@@ -11,21 +11,14 @@ describe('Test the root path', () => {
     expect(res.headers.a).toBe('a')
     expect(body.length).toBe(0)
   })
-  test('It should respond with text body', async () => {
+  test('It should respond with the a body that contains the specified text data', async () => {
     const { body } = await request({
       method: 'get',
       path: '/?query={ "body": {"type": "text", "data": "hello"}}'
     })
     expect(body.toString()).toBe('hello')
   })
-  test('It should respond with the trimmed JSON body', async () => {
-    const { body } = await request({
-      method: 'get',
-      path: '/?query={ "body": {"type": "text", "data": {"name": "echo-server", "author": "yoshipi"}}}'
-    })
-    expect(body.toString()).toBe('{"name":"echo-server","author":"yoshipi"}')
-  })
-  test('It should respond with headers and body', async () => {
+  test('It should respond with the specified headers and body at the same time', async () => {
     const { res, body } = await request({
       method: 'get',
       path: '/?query={ "headers": {"Content-Type": "application/json"}, "body": {"type": "text", "data": {"name": "echo-server", "author": "yoshipi"}}}'
@@ -33,7 +26,14 @@ describe('Test the root path', () => {
     expect(res.headers['content-type']).toBe('application/json')
     expect(body.toString()).toBe('{"name":"echo-server","author":"yoshipi"}')
   })
-  test('It should respond with image body', async () => {
+  test('It should respond with the a body that contains the trimmed JSON data', async () => {
+    const { body } = await request({
+      method: 'get',
+      path: '/?query={ "body": {"type": "text", "data": {"name": "echo-server", "author": "yoshipi"}}}'
+    })
+    expect(body.toString()).toBe('{"name":"echo-server","author":"yoshipi"}')
+  })
+  test('It should respond with a body that contains an image with the specified height and width', async () => {
     const { body } = await request({
       method: 'get',
       path: '/?query={ "body": {"type": "image", "size": {"width": 400, "height": 200}}}'
@@ -42,7 +42,8 @@ describe('Test the root path', () => {
     expect(dimensions.height).toBe(200)
     expect(dimensions.width).toBe(400)
   })
-  test('It should respond with default image body', async () => {
+
+  test('It should respond with a body that contains a default image of 200 in height and width', async () => {
     const { body } = await request({
       method: 'get',
       path: '/?query={ "body": {"type": "image"}}'
@@ -60,14 +61,14 @@ describe('Test the root path', () => {
     expect(res.headers['content-type']).toBe('application/json')
     expect(body.toString()).toBe('{"name":"echo-server","author":"yoshipi"}')
   })
-  test('It should respond with the 200 status code by defualt', async () => {
+  test('It should respond with a status code of 200 by default', async () => {
     const { res } = await request({
       method: 'get',
       path: '/?query={}'
     })
     expect(res.statusCode).toBe(200)
   })
-  test('It works with CORS preflight', async () => {
+  test('It should respond with the specified CORS preflight headers if the request method is OPTIONS and the request headers has the Access-Control-Request-Method and Origin headers', async () => {
     const path = `/?query={
       "headers": {"Access-Control-Allow-Origin": "*", "Content-Type": "application/json"},
       "body": {"type": "text", "data": {"name": "echo-server", "author": "yoshipi"}},
@@ -98,7 +99,7 @@ describe('Test the root path', () => {
     })
     expect(res.statusCode).toBe(200)
   })
-  test('It works with CORS preflight without status', async () => {
+  test('It should respond with the specified CORS preflight headers and a status code of 200 by default if the request method is OPTIONS and the request headers has the Access-Control-Request-Method and Origin headers', async () => {
     const path = `/?query={
       "headers": {"Access-Control-Allow-Origin": "*", "Content-Type": "application/json"},
       "body": {"type": "text", "data": {"name": "echo-server", "author": "yoshipi"}},
