73d405aea54a38b50a7f682ffed93c5279f88cdd
Yoshipi <yoshipyft@gmail.com>
2022-05-22 18:57:24 +0900

    depoly

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,5 +1,5 @@
 import app from './app'
-
-app.listen(5678, () => {
-  console.log('listening at http://localhost:5678')
+const port = process.env.PORT !== undefined ? parseInt(process.env.PORT) : 5678
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`)
 })
