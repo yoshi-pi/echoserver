0d946ebb1c2b478248b87921b1ae0e55bb441d87
Yoshipi <yoshipyft@gmail.com>
2023-05-21 17:30:56 +0900

    Add logic to remove the header if its name is empty, as it causes an error.

--- apps/backend/src/utils.ts
+++ apps/backend/src/utils.ts
@@ -23,24 +23,23 @@ export const getStatusCode = (obj: { [key: string]: unknown }) => {
   }
   return statusCode
 }
-export const getHeaders = (obj: { [key: string]: unknown }, isCorsPreflight: boolean = false) => {
-  if (obj.headers !== undefined) {
-    if (!Array.isArray(obj.headers)) {
-      throw new Error(`the value of headers${isCorsPreflight ? ' of corsPreflight ' : ' '}must be an array`)
-    }
-    obj.headers.forEach(header => {
-      if (!Array.isArray(header)) {
-        throw new Error(`the format of each header${isCorsPreflight ? ' of corsPreflight ' : ' '}must be an array`)
-      }
-      if (header.length < 2) {
-        throw new Error(`each header array length${isCorsPreflight ? ' of corsPreflight ' : ' '}must be at least 2`)
-      }
-      header.forEach(item => {
-        if (typeof item !== 'string') {
-          throw new Error(`elements of header array${isCorsPreflight ? ' of corsPreflight ' : ' '}must be string`)
-        }
-      })
-    })
+export const getHeaders = (obj: { [key: string]: unknown }, isCorsPreflight: boolean = false): string[][] | undefined => {
+  if (obj.headers === undefined) return undefined
+  if (!Array.isArray(obj.headers)) {
+    throw new Error(`the value of headers${isCorsPreflight ? ' of corsPreflight ' : ' '}must be an array`)
   }
-  return obj.headers as string[][] | undefined
+  obj.headers.forEach(header => {
+    if (!Array.isArray(header)) {
+      throw new Error(`the format of each header${isCorsPreflight ? ' of corsPreflight ' : ' '}must be an array`)
+    }
+    if (header.length < 2) {
+      throw new Error(`each header array length${isCorsPreflight ? ' of corsPreflight ' : ' '}must be at least 2`)
+    }
+    header.forEach(item => {
+      if (typeof item !== 'string') {
+        throw new Error(`elements of header array${isCorsPreflight ? ' of corsPreflight ' : ' '}must be string`)
+      }
+    })
+  })
+  return obj.headers.filter(header => header[0] !== '')
 }
