cb30a8ee41d5f1f5899cc375e096d2eaa5b31eeb
Bill Ross <wnross@gwhc.net>
2024-10-17 12:45:19 -0600

    Fixed process signaling

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -3,3 +3,12 @@ const port = process.env.PORT ?? 5678;
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 });
+
+function shutdown(signal: string) {
+    console.log(`Received event: ${signal}`)
+    app.close(()=>{
+      process.exit()
+    })
+}
+process.on('SIGINT',shutdown)   // CTRL-C
+process.on('SIGTERM', shutdown) // docker kill
